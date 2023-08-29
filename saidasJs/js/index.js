6ff9d341b214c9311bf279903271388321b305ed
Aleir Sanches <aleir.sanches@hotmail.com>
2023-08-28 23:45:33 -0300

    atualizando

--- saidasJs/js/index.js
+++ saidasJs/js/index.js
@@ -1,2 +1,8 @@
 document.getElementById("text").innerHTML= "Usando o inner HTML";
 console.log(22*4);
+var  num1 = 88;
+console.log(num1);
+console.log(typeof(num1));
+num1.toString();
+console.log(num1);
+console.log(typeof(num1));
