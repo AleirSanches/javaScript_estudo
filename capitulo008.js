929159b2643925a08318e409af4d20c590ee04d3
Aleir Sanches <aleir.sanches@hotmail.com>
2023-09-13 22:54:27 -0300

    estudando as condicionais

--- capitulo008.js
+++ capitulo008.js
@@ -2,13 +2,20 @@
 
 const adicionaFuncao = new Array("Curitiba"," Florianopolis"," São Paulo"," Porto Alegre");
             adicionaFuncao.push("Bahia");
-            //console.log(adicionaFuncao);
+           
+const idadeComprador = 19;
+const acompamjado = false;
 
+if(idadeComprador >= 18){
+  console.log("Comprador maior de idade dentino : ",adicionaFuncao[0]);
+  adicionaFuncao.splice(0,1) ;    
+}
+else if(acompamjado){
+  console.log("Comprador de menor, porem acomapnhado");
+  adicionaFuncao.splice(0,1) ; 
+}
+else {
+  console.log("Comprador de menor, PROIBIDO VIAJAR");
+}
 
-  //adicionaFuncao.splice(0,2) ;    
-  //console.log(adicionaFuncao);   
-
-  for (var i=0; i< adicionaFuncao.length; i++){
-    console.log(adicionaFuncao[i]);
-
-  };
+  console.log(adicionaFuncao);
